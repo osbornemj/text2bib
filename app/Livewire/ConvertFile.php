50ab90a6ec3e9cbbaee2600da070b2e63da8bd1a
Martin J. Osborne <martin.j.osborne@gmail.com>
2024-01-13 15:03:03 -0500

    Start of conversion of main form to livewire

---
+++ app/Livewire/ConvertFile.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Livewire;
+
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\ErrorReportComment;
+
+use Livewire\Component;
+use Livewire\Attributes\Rule;
+
+class ConvertFile extends Component
+{
+    // #[Rule('required', message: 'Please enter a comment')]    
+    // public $comment;
+
+    // public $comments;
+    // public $errorReportId;
+
+    public function mount()
+    {
+        // $this->comments = ErrorReportComment::where('error_report_id', $this->errorReportId)
+        //     ->orderBy('created_at')
+        //     ->get();
+    }
+
+    public function submit($errorReportId)
+    {
+        // $this->validate();
+        
+        // $user = Auth::user();
+        // $comment = ErrorReportComment::create([
+        //     'error_report_id' => $errorReportId,
+        //     'user_id' => $user->id,
+        //     'comment_text' => $this->comment,
+        // ]);
+
+        // $this->comment = '';
+        // $this->comments = $this->comments->push($comment);
+    }
+
+    public function render()
+    {
+        return view('livewire.convert-file');
+    }
+}
