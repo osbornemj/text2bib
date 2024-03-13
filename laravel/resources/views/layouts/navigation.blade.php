56d5158397cba76babd2088e0342857c97892186
Martin J. Osborne <martin.j.osborne@gmail.com>
2024-03-12 20:01:03 -0400

    Minor change for consistency

--- laravel/resources/views/layouts/navigation.blade.php
+++ laravel/resources/views/layouts/navigation.blade.php
@@ -147,7 +147,7 @@
                         <x-responsive-nav-link :href="route('logout')"
                                 onclick="event.preventDefault();
                                             this.closest('form').submit();">
-                            {{ __('Log Out') }}
+                            {{ __('Log out') }}
                         </x-responsive-nav-link>
                     </form>
                 </div>
