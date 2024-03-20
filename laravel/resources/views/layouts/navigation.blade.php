f6cb4785df013810170091ea8eda2aab2681de28
Martin J. Osborne <martin.j.osborne@gmail.com>
2024-03-19 20:12:17 -0400

    Improve appearance of menu on small screen and show About and Statistics publicly

--- laravel/resources/views/layouts/navigation.blade.php
+++ laravel/resources/views/layouts/navigation.blade.php
@@ -115,10 +115,10 @@
         -->
 
         <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
+        <div class="pt-4 pb-1 bg-gray-200 dark:bg-gray-700">
             @auth
                 <div class="px-4">
-                    <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
+                    <div class="font-medium text-base text-gray-500 dark:text-gray-200">{{ Auth::user()->fullName() }}</div>
                     <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
                 </div>
             @endauth
@@ -171,6 +171,8 @@
             </div>
         </div>
 
+        @auth
+        @else
         <div class="mt-3 space-y-1">
             <x-responsive-nav-link :href="route('login')">
                 {{ __('Log in') }}
@@ -180,5 +182,6 @@
                 {{ __('Register') }}
             </x-responsive-nav-link>
         </div>
+        @endauth
     </div>
 </nav>
