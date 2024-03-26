8f90fd51b76fe2625f7d155138517d38075be87d
Martin J. Osborne <martin.j.osborne@gmail.com>
2024-03-26 18:29:09 -0400

    Debugging code for file deletion

--- laravel/app/Console/Kernel.php
+++ laravel/app/Console/Kernel.php
@@ -32,7 +32,7 @@ class Kernel extends ConsoleKernel
                 $fileModifiedDateTime = Carbon::parse($time);
                 
                 if (Carbon::now()->gt($fileModifiedDateTime->addDays(7))) {
-                    //echo $file."<br>";
+                    echo $file."<br>";
                     Storage::disk("public")->delete($file);
                 }
             }
