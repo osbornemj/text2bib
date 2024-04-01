c8c438e15400b5bc795a1dae19ef4c0dc6015f62
Martin J. Osborne <martin.j.osborne@gmail.com>
2024-04-01 10:59:25 -0400

    Add link to Notepad++

--- laravel/resources/views/livewire/partials/non-utf8-entries.blade.php
+++ laravel/resources/views/livewire/partials/non-utf8-entries.blade.php
@@ -6,7 +6,7 @@
     <div class="sm:px-4 lg:px-4 space-y-6">
         <div class="sm:p-0 pt-0 sm:pt-0">
             <p>
-                The following {{ count($nonUtf8Entries) > 1 ? 'items contain' : 'item contains' }} at least one character (indicated by a question mark) that is not valid utf-8.  One way to fix the problem is to open the file in Notepad++, click on Encoding, and then click on "Convert to UTF-8".
+                The following {{ count($nonUtf8Entries) > 1 ? 'items contain' : 'item contains' }} at least one character (indicated by a question mark) that is not valid utf-8.  One way to fix the problem is to open the file in <x-link href="https://notepad-plus-plus.org/" target="_blank">Notepad++</x-link>, click on Encoding, and then click on "Convert to UTF-8".
             </p>
             <ul class="mt-4 mb-4">
             @foreach ($nonUtf8Entries as $entry)
