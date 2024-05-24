322a72d8603ab0a4ac21f622459e82dbffe078fd
Martin J. Osborne <martin.j.osborne@gmail.com>
2024-05-24 18:18:03 -0400

    Remove basic button

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -1,3 +1,2 @@
 The system is currently not operational.  Unfortunately, I do not know how to fix the error, which appears to be unrelated to the main codebase.  (The system works perfectly on my local system.)  I will continue to work on the problem, but cannot currently predict when I will find a solution.  (Posted May 24, 2024.)
 
-<x-basic-button>Test</x-basic-button>
