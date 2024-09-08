84701d3bc866c1c8ca85b51aa40b8ea6845c6248
Martin J. Osborne <martin.j.osborne@gmail.com>
2024-09-07 21:26:29 -0400

    More small changes

--- laravel/resources/views/about.blade.php
+++ laravel/resources/views/about.blade.php
@@ -8,7 +8,7 @@
             With funding from the Student Experience Program of <x-link href="http://open.utoronto.ca/" target="_blank">Project Open Source | Open Access</x-link> at the University of Toronto, <x-link href="https://ca.linkedin.com/in/fabian-bai-4545ab2b" target="_blank">Fabian Qifei Bai</x-link> created the first version of the conversion script in the Spring of 2007.
         </x-paragraph>
         <x-paragraph class="mt-4">
-            When Bai graduated from the University of Toronto later in 2007, Osborne took over the coding and wrote a front end for public use, using the Open Journals System of the <x-link href="https://pkp.sfu.ca/" target="_blank">Public Knowledge Project</x-link> as a framework.  He has continued to develop both the conversion engine and the front end since then.
+            When Bai graduated from the University of Toronto later in 2007, I took over the coding and wrote a front end for public use, using the Open Journals System of the <x-link href="https://pkp.sfu.ca/" target="_blank">Public Knowledge Project</x-link> as a framework.  I have continued to develop both the conversion engine and the front end since then.
         </x-paragraph>
         <x-paragraph class="mt-4">
             Starting in the summer of 2023, Osborne reimplemented the system using the <x-link href="https://laravel.com" target="_blank">Laravel</x-link> framework, at the same time making many improvements in the conversion engine.  The new version was released on 2024.3.15.
