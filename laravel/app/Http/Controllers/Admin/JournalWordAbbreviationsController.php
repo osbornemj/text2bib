e1087d7192da363b2553833910b1c7f3df23e5c7
Martin J. Osborne <martin.j.osborne@gmail.com>
2024-09-24 11:38:47 -0400

    Extend range

--- laravel/app/Http/Controllers/Admin/JournalWordAbbreviationsController.php
+++ laravel/app/Http/Controllers/Admin/JournalWordAbbreviationsController.php
@@ -116,7 +116,7 @@ class JournalWordAbbreviationsController extends Controller
      */
     public function populate(): RedirectResponse
     {
-        $outputs = Output::where('id', '<', 200000)->where('id', '>=', 190000)->get();
+        $outputs = Output::where('id', '<', 210000)->where('id', '>=', 200000)->get();
         foreach ($outputs as $output) {
             $item = $output->item;
             if (isset($item['journal'])) {
