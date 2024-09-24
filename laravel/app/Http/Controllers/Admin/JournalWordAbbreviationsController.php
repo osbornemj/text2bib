c007ec9ef5d15281dd2efde2c0dde42ba24e3e8c
Martin J. Osborne <martin.j.osborne@gmail.com>
2024-09-24 10:19:53 -0400

    Extend range

--- laravel/app/Http/Controllers/Admin/JournalWordAbbreviationsController.php
+++ laravel/app/Http/Controllers/Admin/JournalWordAbbreviationsController.php
@@ -114,7 +114,7 @@ class JournalWordAbbreviationsController extends Controller
     {
 //        DB::table('outputs')->orderBy('id')->chunk(100, function (Collection $outputs) {
 
-        $outputs = Output::where('id', '<', 120000)->where('id', '>=', 110000)->get();
+        $outputs = Output::where('id', '<', 130000)->where('id', '>=', 120000)->get();
         foreach ($outputs as $output) {
             $item = $output->item;
             if (isset($item['journal'])) {
