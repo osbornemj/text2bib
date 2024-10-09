cb54bc331d553f5350c08d215e4bd2522f4eb316
Martin J. Osborne <martin.j.osborne@gmail.com>
2024-10-09 14:46:23 -0400

    Change colors

--- laravel/resources/views/partials/bstSearchForm.blade.php
+++ laravel/resources/views/partials/bstSearchForm.blade.php
@@ -18,7 +18,7 @@
 
     <div>
         <div class="flex">
-            <x-small-submit-button class="my-2 py-1 bg-sky-700 dark:bg-sky-300">
+            <x-small-submit-button class="my-2 py-1 bg-sky-700 dark:bg-sky-600 dark:text-slate-100">
                 {{ __('Find') }}
             </x-small-submit-button>
         </div>
