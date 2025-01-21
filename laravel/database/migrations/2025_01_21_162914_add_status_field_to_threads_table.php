d3fa86d5cba6aa4ebd3e011707794ba7eb023fa2
Martin J. Osborne <martin.j.osborne@gmail.com>
2025-01-21 16:25:10 -0500

    Correct migration

--- laravel/database/migrations/2025_01_21_162914_add_status_field_to_threads_table.php
+++ laravel/database/migrations/2025_01_21_162914_add_status_field_to_threads_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Enums\CommentStatus;
+use App\Enums\FeedbackThreadStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('threads', function (Blueprint $table) {
-            $table->unsignedInteger('status')->default(CommentStatus::Open->value)->after('title');
+            $table->unsignedInteger('status')->default(FeedbackThreadStatus::Open->value)->after('title');
         });
     }
 
