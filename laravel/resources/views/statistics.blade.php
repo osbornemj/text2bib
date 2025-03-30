72b6a23fa94c31a82159cc2b3fb6cc55cab72820
Martin J. Osborne <martin.j.osborne@gmail.com>
2025-03-30 10:38:32 -0400

    Reorganize stats page

--- laravel/resources/views/statistics.blade.php
+++ laravel/resources/views/statistics.blade.php
@@ -23,12 +23,43 @@
         </x-list>
     </div>
 
+    <div class="mx-4 mt-2">
+        <p>
+            Last 90 days by day: &nbsp;
+            <x-link href="{{ url('#users') }}">Users</x-link>
+            &nbsp;&bull;&nbsp;
+            <x-link href="{{ url('#conversions') }}">Conversions</x-link>
+            &nbsp;&bull;&nbsp;
+            <x-link href="{{ url('#items') }}">Items converted</x-link>
+        </p>
+        <p>
+            By month: &nbsp;
+            <x-link href="{{ url('#usersMonth') }}">Users</x-link>
+            &nbsp;&bull;&nbsp;
+            <x-link href="{{ url('#conversionsMonth') }}">Conversions</x-link>
+            &nbsp;&bull;&nbsp;
+            <x-link href="{{ url('#itemsMonth') }}">Items converted</x-link>
+        </p>
+        <p>
+            <x-link href="{{ url('#intendedUse') }}">Intended use</x-link>
+            &nbsp;&bull;&nbsp;
+            <x-link href="{{ url('#howDiscovered') }}">Discovery method</x-link>
+            &nbsp;&bull;&nbsp;
+            <x-link href="{{ url('statsBibtex') }}">BibTeX style file usage</x-link>
+            &nbsp;&bull;&nbsp;
+            <x-link href="{{ url('statsLanguages') }}">Language selection</x-link>
+            &nbsp;&bull;&nbsp;
+            <x-link href="{{ url('statsCrossref') }}">Crossref usage</x-link>
+        </p>
+    
+    </div>
+
     <h2 class="mx-4 mt-2 font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
         {{ __('Usage statistics by day for last 90 days') }}
     </h2>
 
     <div class="mx-4 mt-2 font-semibold">
-        <h2 class="text-lg">Users</h2>
+        <h2 class="text-lg"><a name="users">Users</a></h2>
 
         <div style="width:100%;">
             {!! $chartjsUsers->render() !!}
@@ -36,7 +67,7 @@
     </div>
 
     <div class="mx-4 mt-4 font-semibold">
-        <h2 class="text-lg">Conversions</h2>
+        <h2 class="text-lg"><a name="conversions">Conversions</a></h2>
 
         <div style="width:100%;">
             {!! $chartjsConversions->render() !!}
@@ -44,7 +75,7 @@
     </div>
 
     <div class="mx-4 mt-4 font-semibold">
-        <h2 class="text-lg">Items converted</h2>
+        <h2 class="text-lg"><a name="items">Items converted</a></h2>
 
         <div style="width:100%;">
             {!! $chartjsItems->render() !!}
@@ -56,7 +87,7 @@
     </h2>
 
     <div class="mx-4 mt-2 font-semibold">
-        <h2 class="text-lg">Users by month</h2>
+        <h2 class="text-lg"><a name="usersMonth">Users by month</a></h2>
 
         <div style="width:100%;">
             {!! $chartjsUsersByMonth->render() !!}
@@ -64,7 +95,7 @@
     </div>
 
     <div class="mx-4 mt-2 font-semibold">
-        <h2 class="text-lg">Conversions by month</h2>
+        <h2 class="text-lg"><a name="conversionsMonth">Conversions by month</a></h2>
 
         <div style="width:100%;">
             {!! $chartjsConversionsByMonth->render() !!}
@@ -72,7 +103,7 @@
     </div>
 
     <div class="mx-4 mt-2 font-semibold">
-        <h2 class="text-lg">Items converted by month</h2>
+        <h2 class="text-lg"><a name="itemsMonth">Items converted by month</a></h2>
 
         <div style="width:100%;">
             {!! $chartjsItemsByMonth->render() !!}
@@ -80,7 +111,7 @@
     </div>
 
     <div class="mx-4 mt-4 font-semibold">
-        <h2 class="text-xl">Conversions by intended use</h2>
+        <h2 class="text-xl"><a name="intendedUse">Conversions by intended use</a></h2>
         (since 2024.4.30)
 
         <div style="width:100%;">
@@ -89,19 +120,11 @@
     </div>
 
     <div class="mx-4 mt-4 font-semibold">
-        <h2 class="text-xl">How have users discovered the site?</h2>
+        <h2 class="text-xl"><a name="howDiscovered">How have users discovered the site?</a></h2>
 
         <div style="width:100%;">
             {!! $chartjsSources->render() !!}
         </div>
-    </div>
-
-    <div class="mx-4 mt-4 font-semibold">
-        <x-link href="{{ url('statsBibtex') }}">BibTeX style file usage</x-link>
-        &nbsp;&bull;&nbsp;
-        <x-link href="{{ url('statsLanguages') }}">Language selection</x-link>
-        &nbsp;&bull;&nbsp;
-        <x-link href="{{ url('statsCrossref') }}">Crossref usage</x-link>
     </div>
 
     <div class="mx-4 mt-4 font-semibold">
