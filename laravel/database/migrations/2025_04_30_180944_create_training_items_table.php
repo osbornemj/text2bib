582fe3d047c59c9ba7bd7381a43ab876fe3971de
Martin J. Osborne <martin.j.osborne@gmail.com>
2025-04-30 19:50:11 -0400

    Adjust training_items table

--- laravel/database/migrations/2025_04_30_180944_create_training_items_table.php
+++ laravel/database/migrations/2025_04_30_180944_create_training_items_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('training_items', function (Blueprint $table) {
             $table->id();
             $table->text('source');
+            $table->string('language', 2)->nullable();
             $table->string('type')->nullable();
             $table->json('item')->nullable();
         });
