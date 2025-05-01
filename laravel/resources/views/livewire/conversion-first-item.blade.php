481915e50f854deb73ddc5b4a49a183e0eda612e
Martin J. Osborne <martin.j.osborne@gmail.com>
2025-05-01 17:51:05 -0400

    Put link only if an item exists

--- laravel/resources/views/livewire/conversion-first-item.blade.php
+++ laravel/resources/views/livewire/conversion-first-item.blade.php
@@ -1,5 +1,7 @@
 <div>
     {{ $conversion->firstOutput()?->source }}
 
-    <x-link wire:click="delete()" class="pl-2 text-red-800 dark:text-red-400 cursor-pointer">Delete item</x-link>
+    @if ($conversion->firstOutput())
+        <x-link wire:click="delete()" class="pl-2 text-red-800 dark:text-red-400 cursor-pointer">Delete item</x-link>
+    @endif
 </div>
