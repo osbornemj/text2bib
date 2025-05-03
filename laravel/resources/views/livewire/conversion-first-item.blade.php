4ae406f00ba7029bfbd98e58ebbd5bf3b34fa64f
Martin J. Osborne <martin.j.osborne@gmail.com>
2025-05-03 15:31:35 -0400

    Remove delete option for lowercase page

--- laravel/resources/views/livewire/conversion-first-item.blade.php
+++ laravel/resources/views/livewire/conversion-first-item.blade.php
@@ -7,9 +7,5 @@
         @endif
     @elseif ($style == 'lowercase')
         {{ $conversion->firstLowercaseOutput()?->source }}
-
-        @if ($conversion->firstLowercaseOutput())
-            <x-link wire:click="delete()" class="pl-2 text-red-800 dark:text-red-400 cursor-pointer">Delete item</x-link>
-        @endif
     @endif
 </div>
