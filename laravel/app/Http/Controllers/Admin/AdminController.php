382111616b67ee1ebeff3cffc9c2b396f40de1bf
Martin J. Osborne <martin.j.osborne@gmail.com>
2026-05-30 20:36:15 -0400

    Removing test timings

--- laravel/app/Http/Controllers/Admin/AdminController.php
+++ laravel/app/Http/Controllers/Admin/AdminController.php
@@ -21,41 +21,26 @@ class AdminController extends Controller
 {
     public function index(): View
     {
-        dump('a');
         $uncheckedBstCount = Bst::where('checked', 0)->count();
 //        $uncheckedJournalCount = Journal::where('checked', 0)->count();
 //        $uncheckedPublisherCount = Publisher::where('checked', 0)->count();
 //        $uncheckedCityCount = City::where('checked', 0)->count();
-        dump('b');
         $uncheckedJournalWordAbbreviationCount = JournalWordAbbreviation::where('checked', 0)->count();
-        dump('c');
         $adminSetting = AdminSetting::first();
-        dump('d');
         $trainingItemsConversionCount = $adminSetting->training_items_conversion_count;
-        dump('e');
         $trainingItemsConversionStartedAt = $adminSetting->training_items_conversion_started_at;
-        dump('f');
         $trainingItemsConversionEndedAt = $adminSetting->training_items_conversion_ended_at;
 
-        dump('g');
         $maxCheckedConversionId = $adminSetting->max_checked_conversion_id;
-        dump('h');
         $uncheckedConversionCount = Conversion::where('id', '>', $maxCheckedConversionId)->count();
-        dump('i');
         $adminCorrectOutputCount = Output::where('admin_correctness', 1)->count();
 
-        dump('j');
         $seconds = Carbon::parse($trainingItemsConversionStartedAt)->diffInSeconds(Carbon::parse($trainingItemsConversionEndedAt));
-        dump('k');
         $itemsPerSecond = $seconds ? number_format($trainingItemsConversionCount / $seconds, 2) : null;
 
-        dump('l');
         $latestVersionRecord = Version::latest()->first();
-        dump('m');
         $latestVersion = $latestVersionRecord ? $latestVersionRecord->created_at : null;
 
-        dump('n');
-        dd('done');
         return view('admin.index', compact(
             'uncheckedBstCount', 
 //            'uncheckedJournalCount', 
